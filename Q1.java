c614b860417bbbddcc2c86b6b9f07e7e2d0679d0
Divya333 <60806494+Divya333@users.noreply.github.com>
2022-11-21 15:34:20 +0530

    Add files via upload

--- Q1.java
+++ Q1.java
@@ -1,1 +1,14 @@
-//Question 1
+import java.util.*;
+public class Main {
+ public static void main(String[] args) {
+ Scanner sc=new Scanner(System.in);
+    int n = sc.nextInt();
+     String result = "";
+ while(n>0){
+ int rem = n%3;
+ n = n/3;
+ result = rem + result;
+}
+System.out.println(result);
+}
+}
